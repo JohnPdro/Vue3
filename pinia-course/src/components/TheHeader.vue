74d4ec105f9761cde0691745462439f12b7e2bac
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2024-09-04 18:29:18 -0300

    Finalizando projeto com Pinia

--- pinia-course/src/components/TheHeader.vue
+++ pinia-course/src/components/TheHeader.vue
@@ -1,6 +1,18 @@
-<script setup>
-// imports
+<!-- Cabeçalho feito com Options API -->
+<script>
+
+import { useAuthUserStore } from '../stores/AuthUserStore';
+import { mapState } from 'pinia';
 import CartWidget from './CartWidget.vue';
+export default {
+  components: { CartWidget },
+  computed: {
+    ...mapState(useAuthUserStore, {
+      user: "username"
+    }),
+  }
+};
+
 </script>
 
 <template>
@@ -9,6 +21,9 @@ import CartWidget from './CartWidget.vue';
     style="background-image: url('/images/double-bubble-outline.png')"
   >
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div>
+      <span class="mr-5">{{ user }}</span>
+      <CartWidget class="inline-block"/>
+    </div>
   </header>
 </template>
